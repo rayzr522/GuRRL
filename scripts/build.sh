5613b95fe6fca3029ff0e4881a3319f08adb99e0
Rayzr522 <rayzr522@gmail.com>
2017-04-12 13:53:50 -0400

    Improved build system

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "$(dirname "$0")/.."
+
+source settings.cfg
+
+PLATFORM="$(uname | tr [:upper:] [:lower:])"
+FILE_NAME="$BOT_NAME-$PLATFORM-$BOT_VERSION"
+
+echo -n "Cleaning old files... "
+rm -rf ./dist
+echo "done"
+
+echo -n "Building main.go... "
+go build -o "dist/$FILE_NAME" ./main.go || (echo "Failed to build"; exit 1)
+echo "done"
+
+echo
+echo "You can now find a built executable in dist/$FILE_NAME"
+echo
